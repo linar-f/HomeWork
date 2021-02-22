adbbdc0779d287d69fffafcbbdddf292ab50bb1c
linar-f <linar-f@yadnex.ru>
2021-02-22 22:43:59 +0500

    initial project version

---
+++ AverageTemperature/AverageTemperature/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AverageTemperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную суточную температуру:");
+            float Tmin = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите максимальную суточную температуру:");
+            float Tmax = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"Среднесуточная температура Т= {(Tmax + Tmin) / 2}");
+            Console.ReadLine();
+        }
+    }
+}
