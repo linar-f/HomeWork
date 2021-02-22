eed593bdd792207b0bcd9ada5e20a232a17a8b54
linar-f <linar-f@yadnex.ru>
2021-02-22 23:35:31 +0500

    Add compare of temperatures

--- AverageTemperature/AverageTemperature/Program.cs
+++ AverageTemperature/AverageTemperature/Program.cs
@@ -10,8 +10,15 @@ namespace AverageTemperature
             float Tmin = Convert.ToSingle(Console.ReadLine());
             Console.WriteLine("Введите максимальную суточную температуру:");
             float Tmax = Convert.ToSingle(Console.ReadLine());
-            Console.WriteLine($"Среднесуточная температура Т= {(Tmax + Tmin) / 2}");
-            Console.ReadLine();
+            if (Tmin<=Tmax)
+            {
+                Console.WriteLine($"Среднесуточная температура Т= {(Tmax + Tmin) / 2}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Данные введены неверно - максимальная температура меньше минимальной!");
+            }
         }
     }
 }
