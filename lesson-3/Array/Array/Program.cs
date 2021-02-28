7e648811f0673e6397e534aa51d73a0e64f80989
linar-f <linar-f@yadnex.ru>
2021-02-28 23:50:44 +0500

    add lesson-3

---
+++ lesson-3/Array/Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] array = new int[3, 3];
+            Console.WriteLine("Введите элементы целочисленного массива 3х3:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Вы ввели массив:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Диагональные элементы этого массива:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(array[i, i]);
+            }
+        }
+    }
+}
