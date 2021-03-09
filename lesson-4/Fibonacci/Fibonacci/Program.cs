64957cd8f9d6e9ecabbcd60a43a58df1ec070282
linar-f <linar-f@yadnex.ru>
2021-03-09 21:16:34 +0500

    Add lesson-4

---
+++ lesson-4/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static long Fibo(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return (n);
+            }
+            else
+            {
+                return (Fibo (n-1) + Fibo (n-2));
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n:");
+            int n = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine($"Значение числа Фибоначчи: {Fibo(n)}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lesson-4/Season/Season/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Season
+{
+    class Program
+    {
+        enum Season
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        }
+        static Season CurrentSeason(int numberMonth)
+            {
+            Season current = Season.Winter;
+            switch (numberMonth)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    current = Season.Winter;
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    current = Season.Spring;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    current = Season.Summer;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    current = Season.Autumn;
+                    break;
+            }
+           return current;
+        }
+
+        static string NameSeason(Season current)
+        {
+            string name = "";
+            switch (current)
+            {
+                case Season.Winter:
+                    name = "зима";
+                    break;
+                case Season.Spring:
+                    name = "весна";
+                    break;
+                case Season.Summer:
+                    name = "лето";
+                    break;
+                case Season.Autumn:
+                    name = "осень";
+                    break;
+            }
+            return name;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер месяца:");
+            int numberMonth = Convert.ToInt32(Console.ReadLine());
+            if (numberMonth > 0 && numberMonth < 13)
+            {
+                Console.WriteLine(NameSeason(CurrentSeason(numberMonth)));
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите число от 1 до 12");
+            }
+            Console.ReadLine();
+        }
+    }
+}
