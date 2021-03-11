f489d8ab1216cef1486ddd4e25338f6dabc0e21e
linar-f <linar-f@yadnex.ru>
2021-03-11 21:51:54 +0500

    Add lesson-5

---
+++ lesson-5/CurrentTime/CurrentTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace CurrentTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textfile = "startup.txt";
+            File.AppendAllText(textfile, $"{DateTime.Now.ToString()} ");
+            Console.ReadLine();
+        }
+    }
+}
